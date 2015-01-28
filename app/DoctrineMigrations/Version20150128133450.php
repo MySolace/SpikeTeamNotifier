afbe6c15ba38704389c9595fd2ad021d9afcaf83
Toshiro Kida <toshiro.kida@gmail.com>
2015-01-28 14:02:14 -0500

    adding migration to cover new settings in DB

--- app/DoctrineMigrations/Version20150128133450.php
+++ app/DoctrineMigrations/Version20150128133450.php
@@ -4,23 +4,65 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use SpikeTeam\SettingBundle\Entity\Setting;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20150128133450 extends AbstractMigration
+class Version20150128133450 extends AbstractMigration implements ContainerAwareInterface
 {
+
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $settingsToSet = array(
+            'alert_timeout' => '24 hours',
+            'twilio_sid' => 'Your Twilio SID',
+            'twilio_token' => 'Your Twilio token',
+            'twilio_number' => 'Your Twilio number',
+            'twilio_message' => 'Default message',
+            'twilio_response' => 'Default response',
+        );
 
+        foreach ($settingsToSet as $key => $value) {
+            $setting = new Setting();
+            $setting->setName($key);
+            $setting->setSetting($value);
+            $em->persist($setting);
+            $em->flush();
+        }
 
     }
 
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $settingsToErase = array(
+            'alert_timeout',
+            'twilio_sid',
+            'twilio_token',
+            'twilio_number',
+            'twilio_message',
+            'twilio_response',
+        );
 
+        foreach ($settingsToErase as $name) {
+            $setting = $em->getRepository('SpikeTeamSettingBundle:Setting')->findOneByName($name);
+            $em->remove($setting);
+            $em->flush();
+        }
 
     }
 }
