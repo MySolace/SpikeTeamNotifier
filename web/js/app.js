2e6c82ae1462b9ecb017d9329bcc174bc505ab1c
Toshiro Kida <toshiro.kida@gmail.com>
2015-01-30 09:17:26 -0500

    finessing UI

--- web/js/app.js
+++ web/js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("body.section-spikers").find("li.spikers").addClass("active");
     $("body.section-admin").find("li.admin").addClass("active");
     $("body.section-settings").find("li.settings").addClass("active");
-    $("#button.enabled").find("img").unwrap();
+    $("#button.enabled").find("button").unwrap();
     $("#button.enabled").click(function(){
         if (confirm("Are you sure you want to notify the Spike Team?") == true) {
             $.ajax({url:"goteamgo"});
@@ -15,5 +15,11 @@ $(document).ready(function(){
     });
     $("button.token-button").click(function(){
         $(".token-para").toggle();
+        $(this).toggleClass("token-showing");
+        if ($(this).hasClass("token-showing")) {
+            $(this).find(".replace").text("hide");
+        } else {
+            $(this).find(".replace").text("view");
+        }
     });
 });
