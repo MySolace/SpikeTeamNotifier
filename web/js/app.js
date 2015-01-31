14a41e3691ddf9b71a16a8ab0fdaff9d5b975538
Toshiro Kida <toshiro.kida@gmail.com>
2015-01-31 10:44:19 -0500

    fixing js bug

--- web/js/app.js
+++ web/js/app.js
@@ -1,17 +1,23 @@
+function goCallback() {
+    $.ajax({url:"goteamgo"});
+}
+
 $(document).ready(function(){
     $("body.section-button").find("li.button").addClass("active");
     $("body.section-spikers").find("li.spikers").addClass("active");
     $("body.section-admin").find("li.admin").addClass("active");
     $("body.section-settings").find("li.settings").addClass("active");
     $("#button.enabled").find("button").unwrap();
-    $("#button.enabled").click(function(){
-        if (confirm("Are you sure you want to notify the Spike Team?") == true) {
-            $(this).removeClass("enabled").addClass("disabled");
-            $.ajax({url:"goteamgo"});
+    $("#button").click(function(){
+        if ($(this).is(".enabled")) {
+            if (confirm("Are you sure you want to notify the Spike Team?") == true) {
+                $(this).removeClass("enabled").addClass("disabled");
+                goCallback();
+            }
         }
-    });
-    $("#button.disabled").click(function(){
-        alert("Sorry, you cannot alert the Spike Team yet.");
+        else if ($(this).is(".disabled")) {
+            alert("Sorry, you cannot alert the Spike Team yet.");            
+        }
     });
     $("button.token-button").click(function(){
         $(".token-para").toggle();
