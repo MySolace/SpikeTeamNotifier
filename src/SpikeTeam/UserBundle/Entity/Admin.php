b6790c86bd650a4f09ad39259b0f2d23fefb26ad
kidat <toshiro.kida@gmail.com>
2015-05-18 11:27:49 -0400

    login via email, not username

--- src/SpikeTeam/UserBundle/Entity/Admin.php
+++ src/SpikeTeam/UserBundle/Entity/Admin.php
@@ -28,6 +28,20 @@ class Admin extends BaseUser
     }
 
     /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=60)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=60)
+     */
+    private $lastName;
+
+    /**
      * Get id
      *
      * @return integer 
@@ -35,6 +49,78 @@ class Admin extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return Spiker
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return Spiker
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    // Override methods for username and tie them with email field
+    // from http://stackoverflow.com/questions/10314932/fosuserbundle-login-with-email-symfony2
+    /**
+     * Sets the email.
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->setUsername($email);
+        return parent::setEmail($email);
+    }
+
+    /**
+     * Set the canonical email.
+     *
+     * @param string $emailCanonical
+     * @return User
+     */
+    public function setEmailCanonical($emailCanonical)
+    {
+        $this->setUsernameCanonical($emailCanonical);
+        return parent::setEmailCanonical($emailCanonical);
     }
 
 }
