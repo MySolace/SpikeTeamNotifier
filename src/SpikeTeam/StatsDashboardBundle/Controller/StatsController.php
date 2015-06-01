e0d3e482c7ff4f10a1d2157ebaa35a51e6996b8e
Andrew Gremmo <andrewjgremmo@gmail.com>
2015-06-01 16:16:38 -0400

    Add post route for stats file

--- src/SpikeTeam/StatsDashboardBundle/Controller/StatsController.php
+++ src/SpikeTeam/StatsDashboardBundle/Controller/StatsController.php
@@ -3,17 +3,34 @@
 namespace SpikeTeam\StatsDashboardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class StatsController extends Controller
 {
     /**
-     * @Route("/stats/")
+     * @Route("/stats/", name="getStats")
+     * @Method("GET")
      * @Template()
      */
     public function statsAction()
     {
+        return array();
+    }
+
+    /**
+     * @Route("/stats/", name="postStats")
+     * @Method("POST")
+     */
+    public function updateStats(Request $request)
+    {
+        $uploadedFile = $request->files->get('stats');
+        $file = $uploadedFile->move('../web/', 'stats.csv');
+
         return array();
     }
 }
