d24b3b1381e8fcc6d209018b7f6f46494f530131
kidatsy <toshiro.kida@gmail.com>
2015-10-29 22:52:31 -0700

    amending migration to add groups up to 7

--- app/DoctrineMigrations/Version20151028100830.php
+++ app/DoctrineMigrations/Version20151028100830.php
@@ -14,19 +14,19 @@ class Version20151028100830 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('ALTER TABLE fos_user CHANGE is_enabled is_enabled TINYINT(1) DEFAULT \'0\' NOT NULL');
         $this->addSql('ALTER TABLE spiker ADD is_captain TINYINT(1) DEFAULT NULL');
         $this->addSql('ALTER TABLE spiker_group ADD captain_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE spiker_group ADD CONSTRAINT FK_9021917C3346729B FOREIGN KEY (captain_id) REFERENCES spiker (id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_9021917C3346729B ON spiker_group (captain_id)');
+        $this->addSql('UPDATE spiker_group SET enabled=1 WHERE enabled=0');
+        $this->addSql('INSERT into spiker_group VALUES (null, "Group 5", 1, null), (null, "Group 6", 1, null), (null, "Group 7", 1, null)');
     }
 
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('ALTER TABLE fos_user CHANGE is_enabled is_enabled TINYINT(1) NOT NULL');
         $this->addSql('ALTER TABLE spiker DROP is_captain');
         $this->addSql('ALTER TABLE spiker_group DROP FOREIGN KEY FK_9021917C3346729B');
